da1ec0a84e10e014840be36e3a806deb906f675a
Harjot Singh Kaur <mrzazon1@gmail.com>
2026-04-19 14:01:58 +0200

    Added Vite Config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        status: resolve(__dirname, 'status/index.html'),
+      },
+    },
+  },
+});
